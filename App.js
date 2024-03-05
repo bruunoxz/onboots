3ed5a45c04a58aeceb642aa1959e9b6fb3422463
Bruno Henrique <135364416+bruunoxz@users.noreply.github.com>
2024-03-04 22:17:12 -0300

    Adicionar primeiro produto

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Logo from './assets/Logo.png'
 import Produto from './src/telas/Produto'
 
 export default function App() {
   return <Produto/>;
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -16,7 +13,7 @@ const styles = StyleSheet.create({
   },
 });
 
-/*<View style={styles.container}>
+<View style={styles.container}>
       <Text>Bem vindo!</Text>
       <Image source={Logo}/>
       <StatusBar style="auto" />
