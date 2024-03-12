b4ac8b4105c142b8ee6521f835734cf3d29036cf
Bruno Henrique <135364416+bruunoxz@users.noreply.github.com>
2024-03-11 22:14:20 -0300

    Adiciona fonte externa no projeto

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import Produto from './src/telas/Produto'
+import { View } from 'react-native'
+import {useFonts, RobotoCondensed_300Light, RobotoCondensed_700Bold} from '@expo-google-fonts/roboto-condensed'
+
 
 export default function App() {
+  //Carrega a fonte
+  const [fonteCarregada] = useFonts({
+    "RobotoCRegular": RobotoCondensed_300Light,
+    "RobotoCBold": RobotoCondensed_700Bold,
+  })
+  //Checa se as fontes já foram carregadas 
+  if(!fonteCarregada){
+    return <View/>
+  }
   return <Produto/>;
 }
 
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-<View style={styles.container}>
-      <Text>Bem vindo!</Text>
-      <Image source={Logo}/>
-      <StatusBar style="auto" />
-    </View>*/
