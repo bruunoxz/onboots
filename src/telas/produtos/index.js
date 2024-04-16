fbab024f66e1cb5f6b2213a2e9627de005ed0bfb
Bruno Henrique <135364416+bruunoxz@users.noreply.github.com>
2024-04-15 21:49:46 -0300

    Organizando a lista

--- src/telas/produtos/index.js
+++ src/telas/produtos/index.js
@@ -3,65 +3,19 @@ import { FlatList, View, Image, Text } from 'react-native';
 
 import Topo from './componentes/Topo'
 import Detalhes from './componentes/Detalhes'
+import Item from './componentes/Item'
 
 export default function Produto({topo, detalhes, itens}) {
 
-  const renderItem = ({item:{nome, imagem}}) => 
-    <View key={nome}>
-      <Image source={imagem}></Image>
-      <Text>{nome}</Text>
-    </View>
-  
-
-  return <>
-    <FlatList
+  return <FlatList
       data={itens.lista}
-      renderItem={renderItem}
-      keyExtractor={({nome})=>{nome}}
+      renderItem={Item}
+      keyExtractor={({id})=>{id}}
+      ListHeaderComponent={()=>{
+        return <>
+          <Topo {...topo}/>
+          <Detalhes {...detalhes}/>
+        </>
+      }}
     />
-      <Topo {...topo}/>
-      <Detalhes {...detalhes}/>
-  </>
 }
-/*
-const styles = StyleSheet.create({
-    campus: {
-      width: "100%",
-      height: 1197 / 1600 * width,
-    },
-    titulo: {
-      width: "100%",
-      position: "absolute",
-      textAlign: "center",
-      fontSize: 20,
-      color: "black",
-      fontWeight: "bold",
-      padding: 8,
-    },
-    produto: {
-      paddingVertical: 8,
-      paddingHorizontal: 16,
-    },
-    nome: {
-      color: "black",
-      fontWeight: "bold",
-      fontSize: 24,
-      paddingTop: 25,
-      paddingLeft: 25
-    },
-    descricao: {
-      fontSize: 18,
-    },
-    preco: {
-      fontSize: 26,
-      fontWeight: "bold",
-      marginTop: 8
-    },
-    logo: {
-      width: 70,
-      height: 70,
-    },
-    logotipo: {
-      flexDirection: "row",
-    }
-  });*/
